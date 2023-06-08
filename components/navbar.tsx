3658034502da7b8ef226472e3676112e96d3ea50
justinexner <92640370+justinexner@users.noreply.github.com>
2023-06-08 23:44:04 +0200

    style: navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import Link from "next/link"
+import Image from "next/image"
+import Logo from "../public/Logo-Cloud.png"
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -14,43 +16,51 @@ import {
 
 export default function Navbar() {
   return(
-    <div className="bg-black">
-      <NavigationMenu className="p-2">
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href={"/"} legacyBehavior passHref>
-              <NavigationMenuLink className="text-white">
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="p-4">Smartphone</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Tablet</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Laptop</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href={"/about"} legacyBehavior passHref>
-              <NavigationMenuLink className="text-white">
-                Über Uns
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
+    <NavigationMenu className="p-2 border-b-2">
+      <NavigationMenuList>
+        <NavigationMenuItem className="grow">
+          <Link href={"/"}>
+            <Image
+              src={Logo}
+              alt="Logo zur Startseite"
+              style={{objectFit: "contain"}}
+              className="w-1/12 pl-2"
+            />
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="pr-12">
+          <Link href={"/"} legacyBehavior passHref>
+            <NavigationMenuLink className="">
+              Home
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Smartphone</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Apple</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Tablet</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Apple</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="grow">
+          <NavigationMenuTrigger>Laptop</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Apple</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem className="pr-4">
+          <Link href={"/about"} legacyBehavior passHref>
+            <NavigationMenuLink className="">
+              Über Uns
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   )
 }
