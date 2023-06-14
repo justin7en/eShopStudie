898cd9eb6bce33ffced92756f88b282e3fc48a75
justinexner <92640370+justinexner@users.noreply.github.com>
2023-06-14 16:29:33 +0200

    feat: Firestore Database connection

--- components/highlightItem.tsx
+++ components/highlightItem.tsx
@@ -1,5 +1,10 @@
+"use client"
+
 import Image from "next/image"
 import Iphone from "../public/IphonePrototyp.png"
+import { useEffect, useState } from "react"
+import { db } from "../firebase"
+import { getDocs, collection, query, where } from "firebase/firestore"
 
 function HighlightItem() {
   return (
@@ -19,6 +24,23 @@ function HighlightItem() {
 }
 
 export default function HighlightItemList() {
+  const [highlightList, setHighlightList] = useState([]);
+
+  const refArtikelCollection = collection(db, "Artikel");
+  const filteredCollection = query(refArtikelCollection, where("Highlight", "==", true))
+
+  useEffect( () => {
+    const getHighlightList = async () => {
+      try {
+        const data = await getDocs(filteredCollection);
+        const filteredData = data.docs.map((doc) => ({...doc.data(), id: doc.id}))
+        console.log(filteredData)
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getHighlightList();
+  });
   return (
     <div className="p-2 space-y-2 overflow-y-auto">
       <HighlightItem />
